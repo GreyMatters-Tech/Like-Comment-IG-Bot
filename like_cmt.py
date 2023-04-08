698bdea0ea2c6fd7113ba1d554e7f713b9770acc
GreyMatter's <greymattersyt@gmail.com>
2023-04-08 18:03:39 +0530

    Create like_cmt.py

---
+++ like_cmt.py
@@ -0,0 +1,39 @@
+import pyautogui as pg
+import keyboard as key
+import time
+import random
+
+time.sleep(3)
+# print(pg.position())
+
+# position of search box: x=643, y=103
+pg.leftClick(643, 103)
+time.sleep(1)
+key.write("#python")
+time.sleep(2)
+
+# position of hashtag; x=650, y=164
+pg.leftClick(650, 164)
+time.sleep(3)
+
+# position of image post x=314, y=529
+pg.leftClick(314, 529)
+time.sleep(3)
+
+list = ["Hello#python", "Hi", "Hey"]
+
+for i in range(5):
+    # position of like button: x=729, y=566
+    time.sleep(1)
+
+    # position of comment button: x=888, y=680
+    pg.leftClick(888, 688, 1)
+    msg = random.choice(list)
+    pg.typewrite(msg)
+    time.sleep(1)
+    pg.press("enter")
+    time.sleep(5)
+
+    # position of next button: x=1322, y=391
+    pg.leftClick(1322, 391)
+    time.sleep(3)
